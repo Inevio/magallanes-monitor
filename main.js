c2e7933e0e3acf8e605030a55a684aa2f530c227
Javier Garmón <jgarmon@inevio.com>
2016-05-08 19:20:56 +0200

    Added server

--- main.js
+++ main.js
@@ -3,8 +3,9 @@
 // Modules
 var vertigo = require('vertigo');
 
-// Create server
-var client = vertigo.createClient({ port : 21042, host : '192.168.0.100' });
+// Create server and client
+var server = vertigo.createServer( 21042 );
+var client = vertigo.createClient({ host : 'magallanes-server', port : 21042 });
 
 // Listen petitions
 module.exports = {
@@ -32,11 +33,5 @@ module.exports = {
   monitorUpdate : require('./cmd/monitorUpdate' ).bind( null, client )
 
 };
-
-/*
-module.exports.registerService( function(){
-  console.log( arguments );
-});
-*/
 
 setInterval( module.exports.monitorUpdate, 10 * 1000 );
