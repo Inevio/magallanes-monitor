770b666a9d81bb9056e81571240d1ceba15b4f70
Javier Garmón <jgarmon@inevio.com>
2016-05-14 02:45:18 +0200

    Fixed little mistake

--- cmd/monitorNode.js
+++ cmd/monitorNode.js
@@ -3,7 +3,7 @@
 // Modules
 var request = require('request');
 
-module.exports = function( client, callback ){
+module.exports = function( callback ){
 
   request.get( 'http://' + process.env.HOST_IP + ':2375/containers/json?all=true', function( error, http, body ){
     callback( error, JSON.parse( body ) );
