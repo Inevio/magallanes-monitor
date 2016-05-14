82861c7f87caf75701046cb72304653443955d4e
Javier Garmón <jgarmon@inevio.com>
2016-05-14 02:41:03 +0200

    Improved monitorNodes

--- cmd/monitorNode.js
+++ cmd/monitorNode.js
@@ -6,7 +6,7 @@ var request = require('request');
 module.exports = function( client, callback ){
 
   request.get( 'http://' + process.env.HOST_IP + ':2375/containers/json?all=true', function( error, http, body ){
-    callback( error, body );
+    callback( error, JSON.parse( body ) );
   });
 
 };
