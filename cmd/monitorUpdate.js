07d51e72069a20c9c625dfe7595ffaa7d0681e58
Javier Garmón <jgarmon@inevio.com>
2016-05-17 01:39:27 +0200

    Improved check interval

--- cmd/monitorUpdate.js
+++ cmd/monitorUpdate.js
@@ -2,8 +2,8 @@
 
 module.exports = function( client ){
 
-  client.request( 'monitorUpdate', process.env.HOST_IP, function( error ){
-    setTimeout( module.exports.bind( null, client ), 10 * 1000 );
-  });
+  setInterval( function(){
+    client.request( 'monitorUpdate', process.env.HOST_IP, function( error ){});
+  }, 10 * 1000 );
 
 };
