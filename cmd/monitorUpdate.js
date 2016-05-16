3c5fd919fb4b59a03b9bbb57edd7b06ae8dc04c9
Javier Garmón <jgarmon@inevio.com>
2016-05-17 01:50:15 +0200

    Fixed undefined variable

--- cmd/monitorUpdate.js
+++ cmd/monitorUpdate.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var update = function(){
-  client.request( 'monitorUpdate', process.env.HOST_IP, function( error ){});
-};
-
 module.exports = function( client ){
+
+  var update = function(){
+    client.request( 'monitorUpdate', process.env.HOST_IP, function( error ){});
+  };
 
   update();
   setInterval( update, 10 * 1000 );
