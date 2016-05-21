d2c165c187e2d9658651b500389b3d928a19732e
Javier Garmón <jgarmon@inevio.com>
2016-05-21 13:42:08 +0200

    Fixed syntax error

--- cmd/monitorKillContainer.js
+++ cmd/monitorKillContainer.js
@@ -2,7 +2,7 @@
 
 module.exports = function( id, callback ){
 
-  request.post({ 'http://' + process.env.HOST_IP + ':2375/containers/' + id + '/kill' }, function ( err, http, body ) {
+  request.post( 'http://' + process.env.HOST_IP + ':2375/containers/' + id + '/kill', function ( err, http, body ) {
     callback( err, body );
   });
 
