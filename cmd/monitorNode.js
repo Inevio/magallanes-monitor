59a55937c3d6b67a4ca76bb5047917bed48866d2
Javier Garmón <jgarmon@inevio.com>
2016-06-02 10:19:13 +0200

    Fixed paths

--- cmd/monitorNode.js
+++ cmd/monitorNode.js
@@ -9,9 +9,9 @@ module.exports = function( callback ){
   if( process.env.HOST_PORT === '2376' ){
 
     req.url  = 'http://' + process.env.HOST_IP + ':2376/containers/json?all=true';
-    req.cert = fs.readFileSync( __dirname + '/cert/cert.pem' );
-    req.key  = fs.readFileSync( __dirname + '/cert/key.pem' );
-    req.ca   = fs.readFileSync( __dirname + '/cert/ca.pem' );
+    req.cert = fs.readFileSync( __dirname + '/../cert/cert.pem' );
+    req.key  = fs.readFileSync( __dirname + '/../cert/key.pem' );
+    req.ca   = fs.readFileSync( __dirname + '/../cert/ca.pem' );
 
   }else{
     req.url = 'http://' + process.env.HOST_IP + ':2375/containers/json?all=true';
