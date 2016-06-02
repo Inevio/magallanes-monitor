96b393ddb7e5bc95656596e27d791744e79da088
Javier Garmón <jgarmon@inevio.com>
2016-06-02 10:26:54 +0200

    Fixed wrong protocol

--- cmd/monitorNode.js
+++ cmd/monitorNode.js
@@ -8,7 +8,7 @@ module.exports = function( callback ){
 
   if( process.env.HOST_PORT === '2376' ){
 
-    req.url  = 'http://' + process.env.HOST_IP + ':2376/containers/json?all=true';
+    req.url  = 'https://' + process.env.HOST_IP + ':2376/containers/json?all=true';
     req.cert = fs.readFileSync( __dirname + '/../cert/cert.pem' );
     req.key  = fs.readFileSync( __dirname + '/../cert/key.pem' );
     req.ca   = fs.readFileSync( __dirname + '/../cert/ca.pem' );
