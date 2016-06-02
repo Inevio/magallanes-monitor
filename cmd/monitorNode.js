77d8e3fefe9fb5104fc5a9913ca01851cfa6222f
Javier Garmón <jgarmon@inevio.com>
2016-06-02 09:34:02 +0200

    Support secure connections in monitorNode

--- cmd/monitorNode.js
+++ cmd/monitorNode.js
@@ -1,8 +1,23 @@
 'use strict';
 
+var fs = require('fs');
+
 module.exports = function( callback ){
 
-  request.get( 'http://' + process.env.HOST_IP + ':2375/containers/json?all=true', function( error, http, body ){
+  var req = {};
+
+  if( process.env.HOST_PORT === '2376' ){
+
+    req.url  = 'http://' + process.env.HOST_IP + ':2376/containers/json?all=true';
+    req.cert = fs.readFileSync( __dirname + '/cert/cert.pem' );
+    req.key  = fs.readFileSync( __dirname + '/cert/key.pem' );
+    req.ca   = fs.readFileSync( __dirname + '/cert/ca.pem' );
+
+  }else{
+    req.url = 'http://' + process.env.HOST_IP + ':2375/containers/json?all=true';
+  }
+
+  request.get( req, function( error, http, body ){
     callback( error, JSON.parse( body ) );
   });
 
